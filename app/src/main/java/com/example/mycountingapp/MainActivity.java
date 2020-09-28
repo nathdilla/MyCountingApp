5c11918f5725bf7bb258e77d5f1c6ad8286499b3
Nathan Dilla <natedillamc@gmail.com>
2020-09-28 13:23:30 -0700

    starts new activity screen 2

--- app/src/main/java/com/example/mycountingapp/MainActivity.java
+++ app/src/main/java/com/example/mycountingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mycountingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,5 +30,10 @@ public class MainActivity extends AppCompatActivity {
     public void onClickSecondButton(View v)
     {
         Toast.makeText(this, "click 2nd button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToScrn2(View v)
+    {
+        startActivity(new Intent(MainActivity.this, Screen2.class));
     }
 }
